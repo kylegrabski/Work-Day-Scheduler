ec55c9dbb960bead0dbd62aa595ce1ca69a364e4
Kyle Grabski <kyle.grabski@gmail.com>
2021-03-05 16:48:29 -0700

    Creating function to populate timeBlocks

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,16 +1,63 @@
-// can assign this as a variable and assign it to our HTML
+// global variables
 
-// creating the current date
-var currentDate = moment().format("dddd, MMMM Do YYYY, h:mm a")
-console.log(currentDate)
+// gets date from moment and format
+var currentDay = moment().format("dddd, MMMM Do YYYY, h:mm a")
+
+
+// variables assigned to HTML elements
 
 // variable linked to our #currentDay HTML element
-// var $currentDay = document.getElementById("currentDay");
 var $currentDay = $('#currentDay');
+var $timeBlock = $('.time-block')
 
-// assigning the text content of our currentDay HTML element
-// to the current date variable
-$currentDay.text(currentDate);
+// show present time upon opening
+$currentDay.text(currentDay);
+// have time update every minute
+
+for (let i = 9; i <= 17; i++) {
+    $timeBlock.append(createTimeBlocks(i));
+    console.log(i)
+}
+    function createTimeBlocks (time) {
+    // create in JS timeblocks from 9a - 5p (9 - 17) ***LOOK AT OUR HTML AND CSS FOR ID/CLASS NAMES***
+    var backgroundColor;
+    if (time < currentDay){
+        backgroundColor = "past";
+    } else if (time === currentDay){
+        backgroundColor = "present";
+    } else {
+        backgroundColor = "future";
+    }
+    var timeBlock = $("<div>");
+
+
+
+
+    // can enter text in each timeblock
+
+    // timeblocks in past have grey background color(.past in CSS)
+
+    // timeblock in present has red background color (.present CSS)
+
+    // timeblocks in future have green background color (.future in CSS)
+
+    // each timeblock has a save button
+
+    // if clicked, the text in the timeblock is saved to local storage.
+
+}
+
+
+
+
+
+
+
+
+    
+
+
+
 
 
 
