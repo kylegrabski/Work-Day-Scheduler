5b2e41bc159ed43576d3f1c32e5cf72fb34d6f6f
Kyle Grabski <kyle.grabski@gmail.com>
2021-03-05 17:04:52 -0700

    now shows time blocks

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,64 +1,49 @@
 // global variables
 
 // gets date from moment and format
-var currentDay = moment().format("dddd, MMMM Do YYYY, h:mm a")
-
-
+var currentDay = moment().format("dddd, MMMM Do YYYY, h:mm a");
+// gets current hour from moment
+var currentHour = moment().format("HH");
+console.log(currentHour)
 // variables assigned to HTML elements
 
 // variable linked to our #currentDay HTML element
-var $currentDay = $('#currentDay');
-var $timeBlock = $('.time-block')
+var $currentDay = $("#currentDay");
+var $timeBlock = $(".time-block");
 
 // show present time upon opening
 $currentDay.text(currentDay);
 // have time update every minute
 
 for (let i = 9; i <= 17; i++) {
-    $timeBlock.append(createTimeBlocks(i));
-    console.log(i)
+  $timeBlock.append(createTimeBlocks(i));
+  console.log(i);
 }
-    function createTimeBlocks (time) {
-    // create in JS timeblocks from 9a - 5p (9 - 17) ***LOOK AT OUR HTML AND CSS FOR ID/CLASS NAMES***
-    var backgroundColor;
-    if (time < currentDay){
-        backgroundColor = "past";
-    } else if (time === currentDay){
-        backgroundColor = "present";
-    } else {
-        backgroundColor = "future";
-    }
-    var timeBlock = $("<div>");
+function createTimeBlocks(time) {
+  // create in JS timeblocks from 9a - 5p (9 - 17) ***LOOK AT OUR HTML AND CSS FOR ID/CLASS NAMES***
 
+  var backgroundColor;
+  if (time < currentHour) {
+    backgroundColor = "past";
+  } else if (time === currentHour) {
+    backgroundColor = "present";
+  } else {
+    backgroundColor = "future";
+  }
+  var timeBlock = $("<div>").addClass("time-block row " + backgroundColor).attr("id", "hour- " + time);
+  
+  
+  
+  return timeBlock;
+  // can enter text in each timeblock
 
+  // timeblocks in past have grey background color(.past in CSS)
 
+  // timeblock in present has red background color (.present CSS)
 
-    // can enter text in each timeblock
+  // timeblocks in future have green background color (.future in CSS)
 
-    // timeblocks in past have grey background color(.past in CSS)
+  // each timeblock has a save button
 
-    // timeblock in present has red background color (.present CSS)
-
-    // timeblocks in future have green background color (.future in CSS)
-
-    // each timeblock has a save button
-
-    // if clicked, the text in the timeblock is saved to local storage.
-
+  // if clicked, the text in the timeblock is saved to local storage.
 }
-
-
-
-
-
-
-
-
-    
-
-
-
-
-
-
-
