f09dd6e11e5462a189fe61caea295e3d328b60cc
Kyle Grabski <kyle.grabski@gmail.com>
2021-03-06 18:00:21 -0700

    text area, buttons visible

--- assets/script/script.js
+++ assets/script/script.js
@@ -17,7 +17,6 @@ $currentDay.text(currentDay);
 
 for (let i = 9; i <= 17; i++) {
   $timeBlock.append(createTimeBlocks(i));
-  console.log(i);
 }
 function createTimeBlocks(time) {
   // create in JS timeblocks from 9a - 5p (9 - 17) ***LOOK AT OUR HTML AND CSS FOR ID/CLASS NAMES***
@@ -34,23 +33,46 @@ function createTimeBlocks(time) {
     // everything left would be in the future, add the 'future' style to variable
     backgroundColor = "future";
   }
-  // creating div elem for our timeBlock, with styles that include the backgroundColor. Adds an id of string 'hour-' as well as the hour it relates to.
+  // creating div elem for our timeBlock
   var timeBlock = $("<div>")
-    .addClass("time-block row " + backgroundColor)
-    .attr("id", "hour- " + time);
+  // add styles that include the backgroundColor. 
+  .addClass("time-block row ")
+  // Adds an id of string 'hour-' as well as the hour it relates to
+    .attr("id", "hour-" + time);
+  // showing the time on the side of the timeBlock
+  var $time = $("<div>").addClass("col-1 hour").text(formatTime(time));
+  // text area for user to type in timeBlock. Possibly put backgrounColor here instead of in the entire timeblock row
+  var $textArea = $("<textarea>").addClass("col-10 description " + backgroundColor);
+  // create the SAVE BUTTONS
+  var $button = $("<button>").addClass("col-1 saveBtn");
 
-  // can enter text in each timeblock
+  // returns timeBlock var data with text area and save button saved to it
+  return timeBlock.append( $time, $textArea, $button);
 
-  // timeblocks in past have grey background color(.past in CSS)
 
-  // timeblock in present has red background color (.present CSS)
+  // function to format the time to 12 hour time
+  function formatTime(time) {
+    if (time > 12){
+      return time - 12 + "PM";
+    }else {
+      return time + "AM";
+    }
+  }
 
-  // timeblocks in future have green background color (.future in CSS)
-
-  // each timeblock has a save button
-
-  // if clicked, the text in the timeblock is saved to local storage.
-
-  // returns timeBlock var data
-  return timeBlock;
+  
 }
+
+
+
+
+// can enter text in each timeblock
+
+// timeblocks in past have grey background color(.past in CSS)
+
+// timeblock in present has red background color (.present CSS)
+
+// timeblocks in future have green background color (.future in CSS)
+
+// each timeblock has a save button
+
+// if clicked, the text in the timeblock is saved to local storage.
